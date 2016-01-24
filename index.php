d3876d69c5ebe00d1d87a5f88c95f0fca8f0c0ed
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-01-24 06:58:25 -0200

    Merge app with portfolio website

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>Crodspring Studio: Mobile Web Design and Development</title>
+    <title>Crodspring Studio: Design and Development for Mobile Web</title>
 
     <!-- Bootstrap core CSS -->
     <link href="assets/css/libs/bootstrap/bootstrap.css" rel="stylesheet">
@@ -22,7 +22,6 @@
 
      <!-- Custom styles for the website -->
 
-    <link href="assets/css/main.css" rel="stylesheet">
     <link href="assets/css/site.css" rel="stylesheet">
     <link href="assets/css/forms.css" rel="stylesheet">
     <link href="assets/css/footer.css" rel="stylesheet">
@@ -40,7 +39,7 @@
 
   <body>
 
-    <nav class="nav navbar-fixed-top">
+    <nav class="nav navbar-fixed-top navbar-inverse">
       <div class="container">
         <div class="navbar-header">
           <a class="navbar-brand" href="#">
@@ -56,7 +55,16 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
             <li><a class="fa fa-home" href="#start">Start</a></li>
-            <li><a class="fa fa-info" href="#portfolio">Portfolio</a></li>
+            <li class="dropdown">
+                  <a class="fa fa-info dropdown-toggle" data-toggle="dropdown" href="#">
+                  Portfolio<span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="fa fa-info" href="#projects">Client Work</a></li>
+                  <li><a class="fa fa-info" href="#demos">Demos</a></li>
+                  <li><a class="fa fa-info" href="#projects">Projects</a></li>
+                </ul>
+            </li>
             <li><a class="fa fa-info" href="#demos">Demos</a></li>
             <li><a class="fa fa-info" href="#projects">Projects</a></li>
             <li><a class="fa fa-folder" href="#mystory">My Story</a></li>
@@ -69,20 +77,20 @@
     </nav><!-- /nav -->
     <div class="container">
       <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <h1>Crodspring Studio:</h1>
+        <h4>The Portfolio of Ismael Rivera </h4>
+        <p class="lead">Design and Development for the Mobile Web</p>
       </div>
     </div><!-- /.container -->
     <div class="csspring_spacer10"></div> 
     <div class="container" id="start">
       <div class="row">
-          <div class="col-md-4"><h3>Start</h3>Veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Qui facit eorum claritatem Investigationes demonstraverunt lectores legere me. Mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc.
-          
-          At vero eros et accumsan et, iusto odio dignissim qui. Nunc nobis videntur parum clari fiant sollemnes in. Minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl. Wisi enim ad ut aliquip ex ea commodo!
-          </div>
-          <div class="col-md-4 col-md-offset-4"><h3>Title</h3>Veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Qui facit eorum claritatem Investigationes demonstraverunt lectores legere me. Mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc.
+          <div class="col-md-4"><h3>Start</h3>I have created websites for Downtown Fort Wayne, Club Soda, Coney Island, Three Rivers Festival, FWDC, and many others over the years. I believe in clean, sleek, and unique designs that speak to the mission of my clients.
+          Joepixel is not your average web development firm. We bring your business dreams into reality. You will receive the best experience, great turn around times and exceptional customer service. We offer a wide range of web services, from simple HTML email, to complex invoicing and payment systems. We use the latest technology to leverage the best possible result for your needs or the needs of your clients.
 
-          At vero eros et accumsan et, iusto odio dignissim qui. Nunc nobis videntur parum clari fiant sollemnes in. Minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl. Wisi enim ad ut aliquip ex ea commodo!
+If you look under the hood of our online media arsenal, you will find powerful technologies such as Flash, ASP, PHP, MySQL, XHTML, HTML5, CSS, XML, Javascript, JSON, AJAX and many search engine optimization (SEO) techniques. We pick and choose amongst the best peforming web technology to maximize your potential.
+          </div>
+          <div class="col-md-4 col-md-offset-4"><h3>Title</h3>I manage the social media accounts for Downtown Fort Wayne, Waiter on the Way, Fox & Fox, Mad Money Coupons, and many more. Social media is about building conversation. When I’m involved, the conversation never stops.
           </div>
       </div><!-- /.row -->
     </div><!-- .container -->
@@ -94,47 +102,47 @@
       </div>  
       <div class="row">
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2 col-md-offset-4">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
 
       </div><!-- /.row -->
       <div class="row">
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2 col-md-offset-4">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
           <div class="col-md-2">
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
-          <a href="#" class="thumbnail"><img src="..." alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
+          <a href="#" class="thumbnail img-responsive"><img src="assets/img/download.svg" alt="thumbnail"></a>
           </div>
       </div><!-- /.row -->
     </div><!-- .container -->
@@ -301,36 +309,43 @@
     </div><!-- .container -->
 
     <div class="csspring_spacer10"></div>
-    <div class="container" id="prefooter"> 
+    <div class="container-fluid" id="prefooter"> 
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-md-2"></div>
+          <div class="col-md-2">
+            <h4>Column 1</h4>
             <ul>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
             </ul>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-2">
+            <h4>Column 1</h4>
             <ul>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
             </ul>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-2">
+            <h4>Column 1</h4>
             <ul>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
-              <li>Linkings of Tahoma</li>            </ul>
+              <li>Linkings of Tahoma</li>            
+            </ul>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-2">
+            <h4>Column 1</h4>
             <ul>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
               <li>Linkings of Tahoma</li>
             </ul>
-          </div> 
-        </div>
+          </div>
+          <div class="col-md-2"></div> 
+        </div><!-- /.row -->
     </div>
     <footer class="footer">
       <div class="container">
